85f5a102c4cd7a0338d3007816823e867f410333
Torben Raab <torben.raab@lila-it.de>
2024-02-28 14:15:35 +0100

    Format and reduce log

--- dist/check-job.sh
+++ dist/check-job.sh
@@ -3,19 +3,23 @@
 jobs=$1
 success_count=0
 
+echo "Jobs to run: $jobs"
+
 while (( success_count < jobs )); do
-  log=$(docker logs gitlab-runner --since 1s)
-  if echo "$log" | grep -q "Failed to process runner"; then
-    echo "Runner failed to process"
-    exit 1
-  fi
-  if echo "$log" | grep -q "Job failed"; then
-    echo "Job failed"
-    exit 1
-  fi
-  new_successes=$(echo "$log" | grep -o "Job succeeded" | wc -l)
-  ((success_count+=new_successes))
-  echo "Jobs completed: $success_count"
+    log=$(docker logs gitlab-runner --since 1s)
+    if echo "$log" | grep -q "Failed to process runner"; then
+        echo "Runner failed to process"
+        exit 1
+    fi
+    if echo "$log" | grep -q "Job failed"; then
+        echo "Job failed"
+        exit 1
+    fi
+    new_successes=$(echo "$log" | grep -o "Job succeeded" | wc -l)
+    ((success_count+=new_successes))
+    if (( new_successes > 0 )); then
+        echo "Jobs completed: $success_count"
+    fi
 done
 
 echo "All $jobs jobs succeeded"
