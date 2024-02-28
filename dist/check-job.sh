900ff94555b7f4df8a24f7f4d15afd300006d068
Torben Raab <torben.raab@lila-it.de>
2024-02-28 14:36:52 +0100

    Update Check Job Script

--- dist/check-job.sh
+++ dist/check-job.sh
@@ -2,11 +2,17 @@
 
 jobs=$1
 success_count=0
+previous_log=""
 
 echo "Jobs to run: $jobs"
 
 while (( success_count < jobs )); do
-    log=$(docker logs gitlab-runner --since 1s)
+    log=$(docker logs gitlab-runner --since 1s 2>&1 | grep -v "gitlab-runner" | grep -v "Checking status of gitlab-runner" | grep -v "Checking for jobs...")
+    if [ "$log" = "$previous_log" ]; then
+        continue
+    fi
+    echo "$log"
+    previous_log="$log"
     if echo "$log" | grep -q "Failed to process runner"; then
         echo "Runner failed to process"
         exit 1
