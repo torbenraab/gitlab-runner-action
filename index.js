0a73c98e9148eca84eeee98b842604820f375f6b
Torben Raab <torben.raab@lila-it.de>
2024-02-28 12:03:52 +0100

    Remove node_modules, update package to use esm, gitlab url input and use new token for runner

--- index.js
+++ index.js
@@ -1,76 +1,78 @@
-const core = require('@actions/core');
-const { exec } = require('@actions/exec');
-const path = require("path");
+import { getInput, setFailed } from "@actions/core";
+import { exec } from "@actions/exec";
+import { resolve } from "path";
 
 async function registerRunnerCmd() {
   let cmdArgs = [];
-  cmdArgs.push(`--rm`)
-  cmdArgs.push(`-v`, `/srv/gitlab-runner/config:/etc/gitlab-runner`)
-  cmdArgs.push(`gitlab/gitlab-runner`)
-  cmdArgs.push(`register`)
-  cmdArgs.push(`--non-interactive`)
-  cmdArgs.push(`--executor`, `docker`)
-  cmdArgs.push(`--docker-image`, core.getInput('docker-image'))
-  cmdArgs.push(`--url`, `https://gitlab.com/`)
-  cmdArgs.push(`--registration-token`, core.getInput('registration-token'))
-  cmdArgs.push(`--name`, core.getInput('name'))
-  cmdArgs.push(`--tag-list`, core.getInput('tag-list'))
-  cmdArgs.push(`--docker-privileged`, true)
-  cmdArgs.push(`--locked="false"`)
-  cmdArgs.push(`--access-level="${core.getInput('access-level')}"`)
-  cmdArgs.push(`--run-untagged="${core.getInput('run-untagged')}"`)
+  cmdArgs.push(`--rm`);
+  cmdArgs.push(`-v`, `/srv/gitlab-runner/config:/etc/gitlab-runner`);
+  cmdArgs.push(`gitlab/gitlab-runner`);
+  cmdArgs.push(`register`);
+  cmdArgs.push(`--non-interactive`);
+  cmdArgs.push(`--executor`, `docker`);
+  cmdArgs.push(`--docker-image`, getInput("docker-image"));
+  cmdArgs.push(`--url`, getInput("gitlab-instance"));
+  cmdArgs.push(`--token`, getInput("token"));
+  cmdArgs.push(`--name`, getInput("name"));
+  cmdArgs.push(`--tag-list`, getInput("tag-list"));
+  cmdArgs.push(`--docker-privileged`, true);
+  cmdArgs.push(`--locked="false"`);
+  cmdArgs.push(`--access-level="${getInput("access-level")}"`);
+  cmdArgs.push(`--run-untagged="${getInput("run-untagged")}"`);
 
-  await exec('docker run', cmdArgs);
+  await exec("docker run", cmdArgs);
 }
 
 async function unregisterRunnerCmd() {
   let cmdArgs = [];
-  cmdArgs.push(`--rm`)
-  cmdArgs.push(`-v`, `/srv/gitlab-runner/config:/etc/gitlab-runner`)
-  cmdArgs.push(`gitlab/gitlab-runner`)
-  cmdArgs.push(`unregister`)
-  cmdArgs.push(`--name`, core.getInput('name'))
+  cmdArgs.push(`--rm`);
+  cmdArgs.push(`-v`, `/srv/gitlab-runner/config:/etc/gitlab-runner`);
+  cmdArgs.push(`gitlab/gitlab-runner`);
+  cmdArgs.push(`unregister`);
+  cmdArgs.push(`--name`, getInput("name"));
 
-  await exec('docker run', cmdArgs);
+  await exec("docker run", cmdArgs);
 }
 
 async function startRunnerCmd() {
-  let cmdArgs = []
-  cmdArgs.push(`-d`)
-  cmdArgs.push(`--name`, `gitlab-runner`)
-  cmdArgs.push(`--restart`, `always`)
-  cmdArgs.push(`-v`, `/srv/gitlab-runner/config:/etc/gitlab-runner`)
-  cmdArgs.push(`-v`, `/var/run/docker.sock:/var/run/docker.sock`)
-  cmdArgs.push(`gitlab/gitlab-runner`)
+  let cmdArgs = [];
+  cmdArgs.push(`-d`);
+  cmdArgs.push(`--name`, `gitlab-runner`);
+  cmdArgs.push(`--restart`, `always`);
+  cmdArgs.push(`-v`, `/srv/gitlab-runner/config:/etc/gitlab-runner`);
+  cmdArgs.push(`-v`, `/var/run/docker.sock:/var/run/docker.sock`);
+  cmdArgs.push(`gitlab/gitlab-runner`);
 
-  await exec('docker run', cmdArgs);
+  await exec("docker run", cmdArgs);
 }
 
 async function stopRunnerCmd() {
-  let cmdArgs = []
-  cmdArgs.push(`gitlab-runner`)
+  let cmdArgs = [];
+  cmdArgs.push(`gitlab-runner`);
 
-  await exec('docker stop ', cmdArgs);
-  await exec('docker rm ', cmdArgs);
+  await exec("docker stop ", cmdArgs);
+  await exec("docker rm ", cmdArgs);
 }
 
-async function checkJob(){
-  await exec(`${path.resolve(__dirname, "dist")}/check-job.sh`)
+async function checkJob() {
+  await exec(`${resolve(__dirname, "dist")}/check-job.sh`);
 }
 
 async function registerRunner() {
-  try{
-    await registerRunnerCmd()
-    await startRunnerCmd()
-    await checkJob()
-  }finally{
-    await unregisterRunner()
+  try {
+    await registerRunnerCmd();
+    await startRunnerCmd();
+    await checkJob();
+  } catch (err) {
+    setFailed(err.message);
+  } finally {
+    await unregisterRunner();
   }
 }
 
 async function unregisterRunner() {
-  await stopRunnerCmd()
-  await unregisterRunnerCmd()
+  await stopRunnerCmd();
+  await unregisterRunnerCmd();
 }
 
-registerRunner()
+registerRunner();
