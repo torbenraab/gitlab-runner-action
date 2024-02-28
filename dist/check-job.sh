2a3aeb047e36b2c2c6c0b67ea431d9dfab9bfaba
Torben Raab <torben.raab@lila-it.de>
2024-02-28 15:06:02 +0100

    Change how docker logs are loaded

--- dist/check-job.sh
+++ dist/check-job.sh
@@ -3,12 +3,11 @@
 jobs=$1
 success_count=0
 previous_log=""
-previous_job_id=""
 
 echo "Jobs to run: $jobs"
 
 while (( success_count < jobs )); do
-    log=$(docker logs gitlab-runner --since 1s 2>&1 | grep -v "gitlab-runner" | grep -v "Checking status of gitlab-runner" | grep -v "Checking for jobs..." | grep -v "Appending trace to coordinator")
+    log=$(docker logs gitlab-runner --tail 1 2>&1 | grep -v "gitlab-runner" | grep -v "Checking status of gitlab-runner" | grep -v "Checking for jobs..." | grep -v "Appending trace to coordinator")
     if [ "$log" = "$previous_log" ]; then
         continue
     fi
@@ -22,11 +21,10 @@ while (( success_count < jobs )); do
         echo "Job failed"
         exit 1
     fi
-    current_job_id=$(echo "$log" | grep -o "job=[0-9]*" | cut -d= -f2)
-    if [ "$current_job_id" != "$previous_job_id" ]; then
-        new_successes=$(echo "$log" | grep -o "Job succeeded" | wc -l)
-        ((success_count+=new_successes))
-        previous_job_id="$current_job_id"
+    new_successes=$(echo "$log" | grep -o "Job succeeded" | wc -l)
+    ((success_count+=new_successes))
+    if (( new_successes > 0 )); then
+        echo "Jobs completed: $success_count"
     fi
 done
 
